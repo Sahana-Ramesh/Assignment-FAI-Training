73f933cf54fe6492b60b02a3a5da6def53e622ce
Sahana-Ramesh <131452818+Sahana-Ramesh@users.noreply.github.com>
2023-04-22 21:03:23 +0530

    Add files via upload

---
+++ assgn1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class assgn1
+    {
+        static void Main(string[] args)
+        {
+            //Range of floating and integral type
+            Console.WriteLine($"The range of byte is from {byte.MinValue} to {byte.MaxValue} ");
+            Console.WriteLine($"The range of short is from {short.MinValue} to {short.MaxValue} ");
+            Console.WriteLine($"The range of int is from {int.MinValue} to {int.MaxValue} ");
+            Console.WriteLine($"The range of long is from {long.MinValue} to {long.MaxValue} ");
+            Console.WriteLine($"The range of double is from {double.MinValue} to {double.MaxValue} ");
+            Console.WriteLine($"The range of float is from {float.MinValue} to {float.MaxValue} ");
+            Console.WriteLine($"The range of decimal is from {decimal.MinValue} to {decimal.MaxValue} ");
+ 
+        }
+    }
+}
