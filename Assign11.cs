40f89d17e36a05a5708e0df7319dcf9afd98850b
Sahana-Ramesh <131452818+Sahana-Ramesh@users.noreply.github.com>
2023-04-30 13:37:00 +0530

    Add files via upload

---
+++ Assign11.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleConApp;
+
+namespace Assignments
+{
+    class Assign11
+    {
+        static void Main(string[] args)
+        {
+            inputValues();
+        }
+
+        private static void inputValues()
+        {
+            Retry:
+            try
+            {
+                
+                double Amt = UIConsole.GetDouble("Enter the Principle ammount:");
+                int term = UIConsole.GetNumber("Enter the term in years:");
+                float rate = UIConsole.GetFloat("Enter the interest rate:");
+
+                InterestCal(Amt, term, rate);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input");
+                goto Retry;
+            }
+        }
+
+        private static void InterestCal(double amt, int term, float rate)
+        {
+            double IntAmt = amt *(1+ (term * (rate/100)));
+            Console.WriteLine("Interest amount: "+ IntAmt);
+        }
+    }
+}
