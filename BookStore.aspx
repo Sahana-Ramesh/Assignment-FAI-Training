68608f0c100d78882f522b141390ee61bd4545ab
Sahana-Ramesh <131452818+Sahana-Ramesh@users.noreply.github.com>
2023-05-15 17:05:32 +0530

    Add files via upload

---
+++ BookStore.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookStore.aspx.cs" Inherits="SampleWebApp.BookStore" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link href="Common/MainStyle.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Book Search</h2>
+            <asp:Label ID="lblSearchCriteria" runat="server" Text="Search Criteria:"></asp:Label>
+            <asp:TextBox ID="txtSearchCriteria" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        </div>
+        <hr />
+        <div>
+            <asp:Repeater ID="rptBooks" runat="server">
+                <ItemTemplate>
+                    <h3><%# Eval("Title") %></h3>
+                    <p><strong>Author:</strong> <%# Eval("Author") %></p>
+                    <p><strong>Genre:</strong> <%# Eval("Genre") %></p>
+                    <p><strong>Publication Year:</strong> <%# Eval("PublicationYear") %></p>
+                    <hr />
+                </ItemTemplate>
+            </asp:Repeater>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ BookStore.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebApp
+{
+    public partial class BookStore : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindBooks();
+            }
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindBooks();
+        }
+
+        private void BindBooks()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string searchCriteria = txtSearchCriteria.Text.Trim();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Title, Author, Genre, PublicationYear FROM Books WHERE Title LIKE '%' + @SearchCriteria + '%' OR Author LIKE '%' + @SearchCriteria + '%'";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SearchCriteria", searchCriteria);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                rptBooks.DataSource = dt;
+                rptBooks.DataBind();
+            }
+        }
+
+    }
+}
